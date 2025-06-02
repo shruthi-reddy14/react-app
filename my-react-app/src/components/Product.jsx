828bf6d00959b8b1a9a5c670192ec8e882d8b9be
shruthi-reddy14 <amireddyshruthi14@gmail.com>
2025-06-02 15:35:45 +0530

    updated

--- my-react-app/src/components/Product.jsx
+++ my-react-app/src/components/Product.jsx
@@ -1,23 +1,38 @@
-
 import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { AppContext } from "../App";
 import axios from "axios";
 export default function Product() {
   const { user } = useContext(AppContext);
-  const [products, setProducts] = useState([]);
-  const fetchProducts = async () => {
+  const [product, setProduct] = useState([]);
+  const fetchProduct = async () => {
     const res = await axios.get("http://localhost:8080/product");
-    setProducts(res.data);
+    setProduct(res.data);
   };
   useEffect(() => {
-    fetchProducts();
+    fetchProduct();
   }, []);
-  return (
+
+// return (
+//     <div>
+//       <h3>Welcome {user.name}! </h3>
+//       Product List
+//       <ul>
+//         {product && product.map((value) => (
+//           <li key={value.name}>
+//             {value.name} - ₹{value.price}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+   return (
     <div>
       <h3>Welcome {user.name}! </h3>
-      Product List
-      {products && products.map((value) => <li>{value.name}</li>)}
-    </div>
-  );
-}
+    Product List
+       {product && product.map((value) => <li>{value.name}-{value.price}</li>)}
+     </div>
+   );
+ }
