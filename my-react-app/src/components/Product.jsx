31d6854dcfe1fa19688e6a2462f5c9ff9e4bfbc2
shruthi-reddy14 <amireddyshruthi14@gmail.com>
2025-06-05 12:26:32 +0530

    updated

--- my-react-app/src/components/Product.jsx
+++ my-react-app/src/components/Product.jsx
@@ -1,43 +1,3 @@
-import React, { useEffect, useState, useContext } from "react";
-import { AppContext } from "../App";
-import axios from "axios";
-
-export default function Product() {
-  const { user } = useContext(AppContext);
-  const [product, setProduct] = useState([]);
-  const API = import.meta.env.VITE_API_URL;
-
-  useEffect(() => {
-    const fetchProduct = async () => {
-      try {
-        const res = await axios.get(`${API}/products/all`);
-        setProduct(res.data.data);
-      } catch (err) {
-        console.error("Error fetching products:", err);
-      }
-    };
-    fetchProduct();
-  }, []);
-
-  return (
-    <div>
-      <h3>Welcome {user?.name || "Guest"}!</h3>
-      <h4>Product List</h4>
-      <ul>
-        {product.length === 0 ? (
-          <li>No products found.</li>
-        ) : (
-          product.map((value) => (
-            <li key={value._id}>
-              {value.name} - ${value.price}
-            </li>
-          ))
-        )}
-      </ul>
-    </div>
-  );
-}
-
 
 // import React, { useEffect, useState } from "react";
 // import { useContext } from "react";
@@ -65,3 +25,41 @@ export default function Product() {
 //      </div>
 //    );
 //  }
+
+import React, { useEffect, useState } from "react";
+import { useContext } from "react"; 
+
+
+
+import { AppContext } from "../App";
+import axios from "axios";
+
+export default function Product() {
+  const { user } = useContext(AppContext);
+  const [products, setProducts] = useState([]);
+  const API = import.meta.env.VITE_API_URL;
+  const fetchProducts = async () => {
+    // const res = await axios.get(`${API}/products/all`);
+    const res = await axios.get(`https://node-app-gamma.vercel.app/products/all`);
+    console.log(res.data);
+    setProducts(res.data);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      <h3>Welcome {user.name}! </h3>
+      <div className="App-Product-Row">
+        {products &&
+          products.map((value) => (
+            <div key={value._id}>
+              <h3>{value.name}</h3>
+              <h4>{value.price}</h4>
+              <button>Add to Cart</button>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
